0c5913a87bfaafa354780b9ff245e5c43f48e975
luoys <luoys@servyou.com.cn>
2020-10-30 16:40:41 +0800

    update

--- src/main/java/com/luoys/upgrade/flagweb/vo/FlagVO.java
+++ src/main/java/com/luoys/upgrade/flagweb/vo/FlagVO.java
@@ -1,10 +1,20 @@
 package com.luoys.upgrade.flagweb.vo;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
 
 @Data
 public class FlagVO {
 
     private String flagId;
-
+    private String flagName;
+    private String flagNameTwo;
+    private String description;
+    @DateTimeFormat(pattern = "YYYY-MM-DD")
+    private Date startDate;
+    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
+    private Date endDate;
 }
