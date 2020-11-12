d87c96af51d3a44987c607fe06262ab273e6e690
luoys <luoys@servyou.com.cn>
2020-11-12 19:08:53 +0800

    查flag详情

--- src/main/java/com/luoys/upgrade/flagweb/vo/FlagQueryVO.java
+++ src/main/java/com/luoys/upgrade/flagweb/vo/FlagQueryVO.java
@@ -1,23 +1,14 @@
 package com.luoys.upgrade.flagweb.vo;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
-import org.springframework.format.annotation.DateTimeFormat;
 
-import java.util.Date;
 
 @Data
 public class FlagQueryVO {
-    private String flagId;
     private String flagName;
-    private String description;
-    private String witnessID;
-    private String witnessName;
+    private String ownerId;
+    private String witnessId;
     private String type;
     private String priority;
-    @DateTimeFormat(pattern = "yyyy-MM-dd")
-    private Date startDate;
-    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
-    private Date endDate;
     private String status;
 }
